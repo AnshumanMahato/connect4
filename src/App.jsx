69418582edc153a592e045e0890275e3d7e0f631
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-10-21 16:00:23 +0530

    headings done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <div>
       <Button small>game</Button>
+      <h1>Heading (L)</h1>
+      <h2>Heading (M)</h2>
+      <h3>Heading (S)</h3>
+      <h4>Heading (XS)</h4>
     </div>
   );
 }
