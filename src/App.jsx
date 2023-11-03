e2e7a885d97978a3f5798e5b498224f029a32dbd
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-11-03 06:05:56 +0530

    timer layout done logic remaining

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainMenu from './components/MainMenu';
 import PauseMenu from './components/PauseMenu';
 import Rules from './components/Rules';
 import ScoreCard from './components/ScoreCard';
+import Timer from './components/Timer';
 
 function App() {
   return (
@@ -23,16 +24,22 @@ function App() {
       <br />
       <br />
       <div>
-        <ScoreCard player="sel" score={12} />
+        <ScoreCard player="player1" score={12} />
       </div>
       <br />
       <br />
       <div>
+        <Timer player="player1" />
+      </div>
+      <br />
+      <br />
+
+      <div>
         <Button small>game</Button>
-        <h1>Heading (L)</h1>
-        <h2>Heading (M)</h2>
-        <h3>Heading (S)</h3>
-        <h4>Heading (XS)</h4>
+        <h1 className="heading--l">Heading (L)</h1>
+        <h2 className="heading--m">Heading (M)</h2>
+        <h3 className="heading--s">Heading (S)</h3>
+        <h4 className="heading--xs">Heading (XS)</h4>
       </div>
     </>
   );
--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,37 +1,70 @@
-import classNames from 'classnames';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import TimerBgRed from '../../assets/images/turn-background-red.svg?react';
+import TimerBgYellow from '../../assets/images/turn-background-yellow.svg?react';
 import './style.scss';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
-function Button({ children, className, red, yellow, small, check }) {
-  const classes = classNames(
-    'btn',
-    {
-      'btn--red': red,
-      'btn--yellow': yellow,
-      'btn--check': check,
-      'btn--small': small,
-    },
-    className
+function Timer({ player }) {
+  let [time, setTime] = useState(15);
+  const interval = useRef(null);
+
+  useEffect(() => {
+    if (time === 0) {
+      clearInterval(interval.current);
+    }
+    console.log(interval.current);
+    if (!interval.current) {
+      console.log('timer started');
+      interval.current = setInterval(() => {
+        console.log('timer tick');
+        setTime((time) => time - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval.current);
+      interval.current = null;
+      console.log('timer cleared');
+    };
+  }, [time]);
+
+  let timerTitle, TimerBackground;
+  switch (player) {
+    case 'cpu':
+      timerTitle = "CPU's Turn";
+      TimerBackground = TimerBgYellow;
+      break;
+    case 'player1':
+      timerTitle = "Player 1's Turn";
+      TimerBackground = TimerBgRed;
+      break;
+    case 'player2':
+      timerTitle = "Player 2's Turn";
+      TimerBackground = TimerBgYellow;
+      break;
+    case 'self':
+      timerTitle = 'Your Turn';
+      TimerBackground = TimerBgRed;
+      break;
+    default:
+      timerTitle = 'Invalid';
+      TimerBackground = TimerBgRed;
+  }
+  return (
+    <div className="timer">
+      <TimerBackground className="timer__background" />
+      <div className="timer__container">
+        <h4 className="timer__title">{timerTitle}</h4>
+        <span className="timer__time">{time}s</span>
+      </div>
+    </div>
   );
-  return <button className={classes}>{check || children}</button>;
 }
 
-Button.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  red: PropTypes.bool,
-  yellow: PropTypes.bool,
-  check: PropTypes.bool,
-  small: PropTypes.bool,
-  checkVariation: ({ red, yellow, small }) => {
-    const count = Number(!!red + !!yellow + !!small);
-    if (count > 1)
-      return new Error('Only one of red, yellow or small can be true');
-  },
-  checkChildren: ({ check, children }) => {
-    if (!check && !children)
-      return new Error('Must have children if not check');
-  },
+Timer.propTypes = {
+  player: PropTypes.oneOf(['self', 'cpu', 'player1', 'player2']).isRequired,
 };
 
-export default Button;
+export default Timer;
