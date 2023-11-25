48f7832160e7d2c8837421fc2131f20e5383f88a
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-11-25 15:37:37 +0530

    grid added

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,11 +1,31 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import BoardBackLarge from '../../assets/images/board-layer-black-large.svg?react';
+import BoardFrontLarge from '../../assets/images/board-layer-white-large.svg?react';
 import './style.scss';
 
 function GameBoard({ className }) {
-  const classes = classNames('game-board', className);
+  const classes = classNames('gameboard', className);
 
-  return <div className={classes}>Gae</div>;
+  const grid = (
+    <div className="gameboard__grid">
+      {[0, 1, 2, 3, 4, 5, 6].map((col) => (
+        <div className="gameboard__grid__col" key={col}>
+          {[0, 1, 2, 3, 4, 5].map((cell) => (
+            <div className="gameboard__grid__cell" key={cell}></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className={classes}>
+      <BoardBackLarge className="gameboard__back" />
+      {grid}
+      <BoardFrontLarge className="gameboard__front" />
+    </div>
+  );
 }
 
 GameBoard.propTypes = {
