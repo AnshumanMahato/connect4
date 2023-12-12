029b7cdeeb5384e762f807f897c7486b5f4e3583
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-12-12 20:05:12 +0530

    counter rerendering solved

--- src/components/GameBoard/components/Counter.jsx
+++ src/components/GameBoard/components/Counter.jsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { memo } from 'react';
 
-function Counter({ counterState }) {
+function CounterComponent({ counterState }) {
   const classes = classNames('gameboard__grid__cell', {
     'gameboard__grid__cell--red': counterState === 1,
     'gameboard__grid__cell--yellow': counterState === 2,
@@ -10,8 +11,10 @@ function Counter({ counterState }) {
   return <div className={classes}></div>;
 }
 
-Counter.propTypes = {
+CounterComponent.propTypes = {
   counterState: PropTypes.number,
 };
+
+const Counter = memo(CounterComponent);
 
 export default Counter;
