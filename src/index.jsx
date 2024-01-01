152a90bffe7f1ea4c355d6c1a7a6a50ce3355f51
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-01-01 15:30:58 +0530

    pages started

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 import './index.scss';
 
 const el = document.getElementById('root');
@@ -9,6 +11,8 @@ const root = ReactDOM.createRoot(el);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/views/Game/index.jsx
@@ -0,0 +1,11 @@
+import './style.scss';
+
+function Game() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Game;
