9c40cee9e0cca6afccad8b64333c3cc4a0d09edd
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-01-26 22:32:46 +0530

    gameboard done

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,18 +1,31 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import CounterInterface from './components/CounterInterface';
+import ControlColumns from './components/ControlColums';
+import CounterGrid from './components/CounterGrid';
 import BoardBackLarge from '../../assets/images/board-layer-black-large.svg?react';
 import BoardFrontLarge from '../../assets/images/board-layer-white-large.svg?react';
+import Result from '../Result';
+import Timer from '../Timer';
+import useGameLogic from './hooks/useGameLogic';
 import './style.scss';
 
 function GameBoard({ className }) {
   const classes = classNames('gameboard', className);
 
+  const { grid, insertCounter } = useGameLogic();
+
+  let winner = 'player1';
   return (
     <div className={classes}>
       <BoardBackLarge className="gameboard__back" />
-      <CounterInterface />
+      <CounterGrid grid={grid} />
       <BoardFrontLarge className="gameboard__front" />
+      <ControlColumns insertCounter={insertCounter} grid={grid} />
+      {winner ? (
+        <Result winner={winner} className="gameboard__status" />
+      ) : (
+        <Timer player="self" className="gameboard__status" />
+      )}
     </div>
   );
 }
