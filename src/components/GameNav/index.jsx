32b2d77b1af86fa10c7b7189ac2a742795a06732
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-02-01 19:37:56 +0530

    Pause Manue integrated

--- src/components/GameNav/index.jsx
+++ src/components/GameNav/index.jsx
@@ -1,18 +1,41 @@
+import { useCallback, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import Button from '../Button';
+import PauseMenu from '../PauseMenu';
 import Logo from '../../assets/images/logo.svg?react';
 import './style.scss';
 
 function GameNav({ className }) {
   const classes = classNames('gamenav', className);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuClick = useCallback(() => setShowMenu(true), []);
+  const handleMenuClose = useCallback(() => setShowMenu(false), []);
 
   return (
-    <nav className={classes}>
-      <Button small>menu</Button>
-      <Logo />
-      <Button small>restart</Button>
-    </nav>
+    <>
+      <nav className={classes}>
+        <Button small onClick={handleMenuClick}>
+          menu
+        </Button>
+        <Logo />
+        <Button small>restart</Button>
+      </nav>
+      <AnimatePresence>
+        {showMenu && (
+          <motion.div
+            className="overlay"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <PauseMenu onClose={handleMenuClose} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
 
--- src/components/PauseMenu/index.jsx
+++ src/components/PauseMenu/index.jsx
@@ -1,16 +1,27 @@
+import PropTypes from 'prop-types';
 import Panel from '../Panel';
 import Button from '../Button';
 import './style.scss';
+import { useDispatch } from 'react-redux';
+import { goToHome } from '../../store';
 
-function PauseMenu() {
+function PauseMenu({ onClose: handleClose }) {
+  const dispatch = useDispatch();
+
+  const handleQuit = () => {
+    dispatch(goToHome());
+  };
+
   return (
     <Panel className="pausemenu">
       <div className="pausemenu__container">
         <h1 className="pausemenu__title">PAUSE</h1>
         <div className="pausemenu__options">
-          <Button className="pausemenu__option">Continue Game</Button>
+          <Button className="pausemenu__option" onClick={handleClose}>
+            Continue Game
+          </Button>
           <Button className="pausemenu__option">Restart</Button>
-          <Button className="pausemenu__option" red>
+          <Button className="pausemenu__option" red onClick={handleQuit}>
             Quit Game
           </Button>
         </div>
@@ -18,5 +29,9 @@ function PauseMenu() {
     </Panel>
   );
 }
+
+PauseMenu.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
 
 export default PauseMenu;
