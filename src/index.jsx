3733e7daac30297b1f3b36867d582bc7e85dbc92
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-02-03 19:41:00 +0530

    scorecards tab queries

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
+import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
-import './index.scss';
 
 const el = document.getElementById('root');
 
