0ed98033396b0e3d2ed7feb98be2aa1557e5c9f2
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-07-17 19:43:37 +0530

    chore: socket.io init

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,43 @@
+import { createServer } from "http";
 import express from "express";
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-app.listen(3000, () => {
+const users = new Map<string, string>();
+
+io.on("connection", async (socket) => {
+  console.log("a user connected");
+
+  socket.on("join", () => {
+    console.log("user joined");
+    const userId = uuidv4();
+    users.set(userId, socket.id);
+    socket.emit("userId", { userId });
+  });
+
+  socket.on("rejoin", (data) => {
+    console.log("user rejoin");
+    users.set(data.userId, socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    users.forEach((value, key) => {
+      if (value === socket.id) {
+        users.delete(key);
+      }
+    });
+  });
+});
+
+server.listen(3000, () => {
   console.log("server started now");
 });
